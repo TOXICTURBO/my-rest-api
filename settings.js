d2185697764c4ae9ec0086af1e1047439f227ea1
Toxic-Turbo <97358745+TOXICTURBO@users.noreply.github.com>
2024-06-10 13:51:18 +0530

    Add files via upload

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
+global.creator = 'TurboMods'
 global.MONGO_DB_URI = "mongodb+srv://turboapis:rPNT0q9q8CG0pU9w@cluster0.x1orsmp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" //database mongodb 
-global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "turboapi730@gmail.com" //email
-global.email_password = "ybjlabpneeiekdcw" //application password email
+global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10"
+global.your_email = "turboapi730@gmail.com"
+global.email_password = "ybjlabpneeiekdcw"
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
